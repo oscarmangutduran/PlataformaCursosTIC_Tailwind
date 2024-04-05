93f787002bf27fa658d39f0506588685b9325560
Oscar Mangut <mangutduranoscar@gmail.com>
2024-04-05 13:41:04 +0200

    Plataforma TIC

---
+++ web-tailwind/tailwind.config.js
@@ -0,0 +1,93 @@
+module.exports = {
+  theme: {
+    extend: {
+      screens: {
+        xs: { max: "768px" },
+      },
+      boxShadow: {
+        header3D:
+          "0px 1px 0px #393d3f, 1px 2px 0px #393d3f, 2px 3px 0px #393d3f, 3px 4px 0px #393d3f",
+        box: "0px 0px 1px rgba(0, 0, 0, 0.3), 0px 3px 7px rgba(0, 0, 0, 0.3), 0px 1px white inset, 0px -3px 1px rgba(0, 0, 0, 0.3) inset",
+      },
+      backgroundImage: (theme) => ({
+        "body-pattern": "url('../img/pattern.png')",
+        banner: "url('../img/bakbaner.png')",
+      }),
+      backgroundPosition: {
+        "banner-position": "-200px -200px",
+      },
+      colors: {
+        "azul-claro": "#37bcf9",
+        "azul-oscuro": "#0370b9",
+      },
+      animation: {
+        "spin-low": "spin 2s linear infinite",
+        "bg-banner": "backBanner 10s linear",
+        "text-banner": "showBannerText 10s linear",
+        "show-card-icon": "showCardIcon 300ms linear",
+        "show-card-category": "showCardCategory 300ms linear",
+        "show-card-description": "showCardDescription 300ms linear",
+      },
+      keyframes: {
+        backBanner: {
+          "0%": { "background-position": "0px 0px" },
+          "100%": { "background-position": "-200px -200px" },
+        },
+        showBannerText: {
+          "0%": {
+            transform: "translateX(-600%) scale(5,5)",
+            opacity: 1,
+          },
+          "50%": {
+            transform: "translateX(600%) scale(5,5)",
+            opacity: 1,
+          },
+          "75%": {
+            transform: "translateX(-600%) scale(5,5)",
+            "text-shadow": "none",
+            color: "transparent",
+            opacity: 0,
+          },
+          "100%": {
+            transform: "translateX(0%) scale(1,1)",
+            "text-shadow": "2px 2px 1px black",
+            color: "white",
+            opacity: 1,
+          },
+        },
+        showCardIcon: {
+          "0%": {
+            transform: "translateY(-200%)",
+          },
+          "100%": {
+            transform: "translateY(0%)",
+          },
+        },
+        showCardCategory: {
+          "0%": {
+            transform: "translateY(-300%)",
+          },
+          "100%": {
+            transform: "translateY(0%)",
+          },
+        },
+        showCardDescription: {
+          "0%": {
+            transform: "translateX(-300%)",
+          },
+          "100%": {
+            transform: "translateX(0%)",
+          },
+        },
+      },
+    },
+  },
+  variants: {
+    animation: ["responsive", "hover", "group-hover"],
+    animate: ["responsive", "hover", "group-hover"],
+    fontSize: ["responsive", "hover", "group-hover"],
+    transform: ["responsive", "hover", "group-hover"],
+    scale: ["responsive", "hover", "group-hover"],
+    padding: ["responsive", "hover", "group-hover"],
+  },
+};
